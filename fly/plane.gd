8c8fddf3c20da1f5cb6b74c8877f3ad1c249b3ec
valkyrie_pilot <valk@randomairborne.dev>
2024-09-22 12:24:30 -0600

    add bigger ground

--- fly/plane.gd
+++ fly/plane.gd
@@ -3,5 +3,4 @@ extends RigidBody2D
 const BALANCE_FORCE = Vector2(-50, -9807)
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	
 	state.apply_central_force(BALANCE_FORCE.rotated(rotation))
