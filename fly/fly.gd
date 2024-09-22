773c30a7b977539016bd9b34043777fe4916415e
valkyrie_pilot <valk@randomairborne.dev>
2024-09-22 12:20:21 -0600

    add planes

---
+++ fly/fly.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+const SPAWN_OFFSETS = 256
+const SPAWN_CHANCE = 100
+var spawnedPlaces = {}
+var planeScene = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	planeScene = load("res://fly/Plane.tscn")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	var rocket_position = ($Rocket).position
+	var spawn_center = Vector2(round_spawn_offset(rocket_position.x), round_spawn_offset(rocket_position.y))
+	for y in range(round_spawn_offset(spawn_center.y-screen_size.y), spawn_center.y + screen_size.y, SPAWN_OFFSETS):
+		if y > 0 - SPAWN_OFFSETS:
+			continue
+		for x in range(round_spawn_offset(spawn_center.x-screen_size.x), spawn_center.x + screen_size.x, SPAWN_OFFSETS):
+			var spawn_position = Vector2(x, y)
+			if spawnedPlaces.has(spawn_position):
+				continue
+			spawnedPlaces[spawn_position] = true
+			if randi_range(0, SPAWN_CHANCE) == 0:
+				var new_plane = planeScene.instantiate()
+				new_plane.name = "Plane %i %i" % [spawn_position.x, spawn_position.y]
+				new_plane.position = spawn_position
+				get_tree().root.add_child(new_plane)
+				
+	pass
+
+func round_spawn_offset(number: float) -> float:
+	return SPAWN_OFFSETS * round(number / SPAWN_OFFSETS)
---
+++ fly/plane.gd
@@ -0,0 +1,7 @@
+extends RigidBody2D
+
+const BALANCE_FORCE = Vector2(-50, -9807)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	
+	state.apply_central_force(BALANCE_FORCE.rotated(rotation))
